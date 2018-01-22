c229d3456ede8d9fa5fbb3f7046add13d4f090f2
lucas suarez <lucas.suarez.utepsa@gmail.com>
2018-01-22 17:38:58 -0400

    Modificaciones en las modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,13 @@ import { NgModule } from '@angular/core';
 // router
 import {APP_ROUTES} from './app.routes';
 
+// modulos
+import { PagesModule } from "./pages/pages.module";
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 
 
@@ -23,19 +19,12 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HeaderComponent } from "./header/header.component";
+import { NopagefoundComponent } from "../pages/nopagefound/nopagefound.component";
+
+
+
+
+
+@NgModule({
+
+  declarations:[
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+
+  ],
+  exports:[
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ]
+})
+
+export class SharedModule {}
